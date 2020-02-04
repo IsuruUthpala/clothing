cb4db7c5b4ffee86340f1d9162dfccd9d8b0e345
Isuru Uthpala <isuruuthpalaiup@gmail.com>
2020-02-04 20:49:00 +0530

    collection preview item added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
-import HatsPage from "./pages/hats/hatspage.component";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route path="/shop/hats" component={HatsPage} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
 }
